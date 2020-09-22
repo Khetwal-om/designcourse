2d8f9fb624e7e796ba9bc86a5422c9fe300d6d93
khetwal-om <khetwalom@gmail.com>
2020-09-22 15:51:04 +0530

    6 Scrolling navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,55 @@
+import React, { Component } from 'react'
+
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+
 import './Header.css'
 import logo from '../images/browsing.svg'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup"> 
-          <Link to="/"> <img src={logo} width="30"></img> </Link>
-          <Link to="/courses">Courses</Link>
-          <Link to="/downloads">downloads</Link>
-          <Link to="/workshop">workshop</Link>
-          <Link to="/buy"><button>Buy</button></Link>
+
+export class Header extends Component {
+  
+    state = {
+       hasScrolled:false
+    }
+  
+  componentDidMount(){
+    window.addEventListener('scroll',this.handleScroll)
+  }
+   
+  handleScroll=(event)=>{
+    const scrollTop=window.pageYOffset
+    if(scrollTop>50){
+      this.setState({
+        hasScrolled: true
+      })
+    }
+    else{
+      this.setState({
+        hasScrolled: false
+      })
+    }
+  }
+  
+  render() {
+    return (
+      <div className={this.state.hasScrolled? 'Header HeaderScrolled':'Header'}>
+      <div className="HeaderGroup"> 
+            <Link to="/"> <img src={logo} width="30"></img> </Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/downloads">downloads</Link>
+            <Link to="/workshop">workshop</Link>
+            <Link to="/buy"><button>Buy</button></Link>
+      </div>
+  
     </div>
+  
+    )
+  }
+}
 
-  </div>
 
-)
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
